5b3cccc6f9933b6e71a1e1b3254ff12a4929fcdd
Brady-Brandt <bradybrandt3@gmail.com>
2024-09-25 13:16:23 -0500

    Initialize memory

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "interpret.h"
 #include "tokenizer.h"
 #include "parser.h"
+#include "arena.h"
 #include "file.h"
 
 
@@ -13,6 +14,8 @@ int main(int argc, char** argv){
         fprintf(stderr,"Invalid number of args\n");
         return 1; 
     }
+
+    memory_init();
   
     File file = file_open(argv[1]);
     ArrayList* tokens = tokenize_file(&file);
@@ -20,6 +23,8 @@ int main(int argc, char** argv){
     parser_create(&p, &file, tokens);
     ArrayList* stmts = parse_tokens(&p);
     interpt_stmts(&file, stmts);
+
+    memory_cleanup();
 
     return 0;
 }
