06a6ec6b0c0e1d2334f86d96a0e2162bed119398
Brady-Brandt <bradybrandt3@gmail.com>
2025-01-20 14:20:26 -0600

    Stacks are no longer arraylist wrappers

--- src/stack.h
+++ src/stack.h
@@ -1,26 +1,41 @@
 #pragma once 
 
-#include "arraylist.h"
-#include <stdbool.h>
+#include <stdint.h>
 
-typedef ArrayList Stack;
+typedef struct {
+    uint32_t size;
+    uint32_t cap;
+    void* data;
+} Stack;
 
 
-#define stack_create(s, type, size) array_list_create_cap(s, type, size)
+#define stack_create(stack, type, capacity) \
+do { \
+    stack = const_pool_alloc(sizeof(Stack) + sizeof(type) * capacity); \
+    stack->size = 0; \
+    stack->cap = capacity; \
+    stack->data = ((char*)stack + sizeof(Stack)); \
+} while(0) 
 
 
 #define stack_is_empty(s) (s->size == 0) 
 
+#define stack_is_full(s) (s->size == s->cap)
+
+#define stack_size(s) (s->size)
+
+#define stack_get(s, type, index) ((type*)(s->data))[index]
+
 #define stack_push(s, type, item) \
     do { \
-        if(s->size != s->capacity){ \
-            array_list_append(s, type, item); \
+        if(s->size != s->cap){ \
+            ((type*)(s->data))[s->size++] = item; \
         } \
     } while(0) \
 
 
-#define stack_pop(s, type) array_list_get(s, type, --s->size)
+#define stack_pop(s, type) ((type*)(s->data))[--s->size]
 
-#define stack_peek(s, type) array_list_get(s, type, s->size - 1)
+#define stack_peek(s, type) ((type*)(s->data))[s->size - 1]
 
-#define stack_delete(s) array_list_delete(s)
+
